c2bcfc0ebc2cb58b7e67351dd1632cafd57dc810
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-04 22:35:53 -0500

    Completed the string reverser and started the number factorial

---
+++ JavaScript/basicAlgorithmScripting/factorializeANumber.js
@@ -0,0 +1,10 @@
+/* 
+
+INPUT:      Given integer.
+TASK:       Reverse given string.
+OUTPUT:     Reversed string.
+
+AUTHOR:     Aakash Sudhakar.
+SOURCE:     Algorithm Challenges on freeCodeCamp.
+
+*/
